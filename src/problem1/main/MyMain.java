cb061c0e29bd3ce527a5857d7d28c9dd11426ab9
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-03-27 23:44:32 +0530

    Update MyMain.java

--- src/problem1/main/MyMain.java
+++ src/problem1/main/MyMain.java
@@ -6,20 +6,27 @@
  */
 package problem1.main;
 // executable class
-import  problem1.mybst.MyBinarySearchTree;
+import problem2.main.binarySearchTree.MyBinarySearchTree;
+import problem4.myqueue.MyQueue;
+
+// executable class
+// use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
-  public static void main(String args[])
-  {
-    MyBinarySearchTree obj=new MyBinarySearchTree();
-    obj.insert(5);
-    obj.insert(1);
-    obj.insert(10);
-    obj.insert(4);
-    obj.insert(6);
-    System.out.println("All Left Nodes");
-    obj.printLeftNode();
-    System.out.println("Count of Nodes not having left node");
-    obj.countNotLeft();
-  }
-    
+    public static void main(String[] args) {
+        MyQueue preQueue = new MyQueue();
+        MyQueue postQueue= new MyQueue();
+        MyBinarySearchTree myBinarySearchTree = new MyBinarySearchTree(preQueue,postQueue);
+        myBinarySearchTree.insert(15);
+        myBinarySearchTree.insert(10);
+        myBinarySearchTree.insert(50);
+        myBinarySearchTree.insert(55);
+        myBinarySearchTree.insert(60);
+        myBinarySearchTree.postOrder(myBinarySearchTree.getRoot());
+        myBinarySearchTree.preOrder(myBinarySearchTree.getRoot());
+        System.out.println("PreOrder:");
+        preQueue.show();
+        System.out.println("PostOrder:");
+        postQueue.show();
+        System.out.println();
+    }
 }
