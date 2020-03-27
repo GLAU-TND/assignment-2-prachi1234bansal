533cc8253a1e1171b1362a2257a23fc332a2a32d
prachi1234bansal <52649082+prachi1234bansal@users.noreply.github.com>
2020-03-27 23:25:49 +0530

    Update MyMain.java

--- src/problem3/main/MyMain.java
+++ src/problem3/main/MyMain.java
@@ -5,7 +5,18 @@
  *  Time: 7:13 PM
  */
 package problem3.main;
+import problem3.myqueue.MyPriorityQueue;
 // executable class
 // use problem5.student.Student class to create object of student
 public class MyMain {
+  public static void main(String args[])
+  {
+    MyPriorityQueue obj=new PriorityQueue();
+    obj.insert("kopl",1);
+    obj.insert("mahi",4);
+    obj.insert("poli",3);
+    obj.insert("iol",2);
+    obj.insert("john",5);
+    obj.display();
+  }
 }
